2f204dd02013e49c8c95761be68c1b224d373642
Tamir Duberstein <tamird@gmail.com>
2026-01-15 13:45:25 -0500

    scripts: generate_rust_analyzer.py: rename `cfg`
    There are many variables with names containing "cfg", which is quite
    confusing. Thus rename this one `generated_cfg` to clarify its meaning.
    
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -23,12 +23,12 @@ def args_crates_cfgs(cfgs):
 
 def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs, sysroot_edition):
     # Generate the configuration list.
-    cfg = []
+    generated_cfg = []
     with open(objtree / "include" / "generated" / "rustc_cfg") as fd:
         for line in fd:
             line = line.replace("--cfg=", "")
             line = line.replace("\n", "")
-            cfg.append(line)
+            generated_cfg.append(line)
 
     # Now fill the crates list -- dependencies need to come first.
     #
@@ -162,7 +162,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs, sysroot_e
             display_name,
             srctree / "rust" / display_name / "lib.rs",
             deps,
-            cfg=cfg,
+            cfg=generated_cfg,
         )
         crate["env"]["OBJTREE"] = str(objtree.resolve(True))
         crate["source"] = {
@@ -205,7 +205,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs, sysroot_e
                 name,
                 path,
                 ["core", "kernel"],
-                cfg=cfg,
+                cfg=generated_cfg,
             )
 
     return crates
